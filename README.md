6287b58872c17cec4057d6b616bc84a089b0cc8c
sivabalaji29 <sivabalaji10000@gmail.com>
2026-02-01 19:18:43 +0530

    Add Azure Kubernetes services(AKS) cluster creation module

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 In this project we use Terraform to automate AWS and Azure cloud resources parallely using Terraform configuration.
  
  
-## Build multi-cloud networking resources in Azure and AWS using Terraform
+## Build multi-cloud Kubernetes cluster in Azure and AWS using Terraform
 ```bash
 # Switch to Terraform project directory.
 $ cd <PROJECT_ROOT_DIR>
@@ -16,3 +16,4 @@ $ terraform plan -var-file var/PRD.tfvars -out plan/PRD.tfplan
 # Apply Terraform plan.
 $ terraform apply plan/PRD.tfplan
 ```
+ 
--- main.tf
+++ main.tf
@@ -12,13 +12,25 @@ terraform {
   backend "local" {}
 }
  
-module "aws_vpc" {
-  source = "./module/aws/vpc"
-  environment = var.environment
-}
+// module "aws_vpc" {
+//   source = "./module/aws/vpc"
+//   environment = var.environment
+// }
  
-module "azure_network" {
-  source = "./module/azure/network"
+// module "azure_network" {
+//   source = "./module/azure/network"
+//   environment = var.environment
+//   subscription_id = var.azure_subscription_id
+// }
+
+
+// module "aws_kubernetes" {
+//   source = "./module/aws/kubernetes"
+//   environment = var.environment
+// }
+ 
+module "azure_kubernetes" {
+  source = "./module/azure/kubernetes"
   environment = var.environment
   subscription_id = var.azure_subscription_id
 }
